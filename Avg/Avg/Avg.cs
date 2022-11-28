1f302249036f3fdc8e5ae780566a8d78fa59415d
MahakkapanPlangklang <119285823+MahakkapanPlangklang@users.noreply.github.com>
2022-11-28 17:14:28 +0700

    Update

--- Avg/Avg/Avg.cs
+++ Avg/Avg/Avg.cs
@@ -1,27 +1,27 @@
-﻿using Work;
+﻿using Work1;
 
 Student student1 = new Student();
 student1.name = "Beer";
-student1.weight = 60.8f;
+student1.weight = 80.0f;
 
 Student student2 = new Student();
 student2.name = "Mark";
 student2.weight = 73.0f;
 
 Student student3 = new Student();
-student3.name = "Phanu";
-student3.weight = 65.4f;
+student3.name = "Big";
+student3.weight = 65.0f;
 
 Student student4 = new Student();
 student4.name = "Tira";
-student4.weight = 70.2f;
+student4.weight = 75.0f;
 
 Student student5 = new Student();
 student5.name = "Na";
-student5.weight = 40.5f;
+student5.weight = 90.0f;
 
-float avgweight = (student1.weight + student5.weight + student3.weight + student4.weight + student5.weight) / 5;
-Console.WriteLine("ค่าเฉลี่ยน้ำหนักของนักศึกษาทั้งหมด ==> " + " " + avgweight);
-Console.WriteLine("ชื่อและน้ำหนักของคนที่มีน้ำหนักมากที่สุด ==> " + " " + student4.name + " " + student4.weight);
-Console.WriteLine("ชื่อและน้ำหนักของคนที่มีน้ำหนักน้อยทีสุด ==> " + " " + student2.name + " " + student2.weight);
-Console.WriteLine("ชื่อ&รหัส ของผู้พัฒนาโปรแกรม ==> " + " " + "มหัคฆพันธ์ ปลั่งกลาง \n 653450296-6");
+float avgweight = (student1.weight + student2.weight + student3.weight + student4.weight + student5.weight) / 5;
+Console.WriteLine("Avg weight of all students:" + " " + avgweight);
+Console.WriteLine("Name and weight of the heaviest person:" + " " + student5.name + " " + student5.weight);
+Console.WriteLine("Name and weight of the person with the least weight:" + " " + student3.name + " " + student3.weight);
+Console.WriteLine("Name and StudentId of Developer:" + " " + "Mahakkapan Plangklang \n 653450296-6");
